dc71a98df095a6b825a7061000667e3bbaf7973a
Miriam2509 <43902062+Miriam2509@users.noreply.github.com>
2021-10-27 21:10:23 +0200

    subido el inicio del diseño register

--- app/src/main/java/ucm/appmenus/MainActivity.java
+++ app/src/main/java/ucm/appmenus/MainActivity.java
@@ -1,6 +1,9 @@
 package ucm.appmenus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,6 +13,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.ArrayList;
+
+import ucm.appmenus.login.RegistroActivity;
+import ucm.appmenus.ui.filtros.FiltrosRecyclerAdapter;
+import ucm.appmenus.ui.filtros.FiltrosRestauranteActivity;
+
+
 public class MainActivity extends AppCompatActivity {
 
     //En principio no hay que hacer nada mas en esta actividad ya que tod0 se hace en los fragments
@@ -17,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //ACCION BOTON DE LA PAGINA LOGIN
+
+      final  Button botonRegistro = findViewById(R.id.boton_registro);
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                //Buscar resultados y abrir activity
+                Intent intent = new Intent(botonRegistro.getContext(), RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
         /*
         BottomNavigationView navView = findViewById(R.id.nav_view);
         //Carga la vista de la barra inferior con las 3 ventanas que contiene
---
+++ app/src/main/java/ucm/appmenus/login/RegistroActivity.java
@@ -0,0 +1,14 @@
+package ucm.appmenus.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ucm.appmenus.R;
+import android.os.Bundle;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
