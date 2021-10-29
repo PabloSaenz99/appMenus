e3dcbd1c9f1d42dda00b6b53acfcfb572b05a2dc
Miriam2509 <43902062+Miriam2509@users.noreply.github.com>
2021-10-29 07:57:26 +0200

    iniciosesion terminado

--- app/src/main/java/ucm/appmenus/MainActivity.java
+++ app/src/main/java/ucm/appmenus/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_iniciosesion);
         //setContentView(R.layout.activity_registro);
         //ACCION BOTON DE LA PAGINA LOGIN
 
