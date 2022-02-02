8b3615fcf15c233759a26b2617e8eaadb17a5939
PabloSaenz99 <60698400+PabloSaenz99@users.noreply.github.com>
2022-02-02 19:59:03 +0100

    Link util de stackoverflow

--- app/src/main/java/ucm/appmenus/utils/WebScrapping.java
+++ app/src/main/java/ucm/appmenus/utils/WebScrapping.java
@@ -2,8 +2,6 @@ package ucm.appmenus.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
@@ -15,9 +13,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashSet;
 
 import ucm.appmenus.entities.Restaurante;
