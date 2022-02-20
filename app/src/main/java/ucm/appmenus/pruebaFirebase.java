bf703aa4447194aafb1b893c7a0809c98c5eb262
Miriam2509 <43902062+Miriam2509@users.noreply.github.com>
2022-02-20 21:03:51 +0100

    cambios para firebase
    he metido cambios en el gradle para que se conecte con firebase la bbdd

---
+++ app/src/main/java/ucm/appmenus/pruebaFirebase.java
@@ -0,0 +1,14 @@
+package ucm.appmenus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pruebaFirebase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba_firebase);
+    }
+}
